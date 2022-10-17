9ba8fe6b2313cb30b8eb52d0b04b41c8b57ccf27
etdnp <etdn.prod@gmail.com>
2022-10-17 08:27:35 +0200

    front end

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ import { NavbarNotLogged } from "./Navbar";
 import Navbar from "./Navbar";
 import results from "./img/results.png";
 import my_avatar from "./img/avatar5.png";
-
+import Slider from "../components/Slider";
 
 export default function Home({ currentUser }) {
   return (
@@ -45,59 +45,61 @@ export default function Home({ currentUser }) {
             <br />
             <br />
             <div className="container result1">
-          <h2 className="quiz_title">[insert quiz title]</h2>
-          <img className="my_avatar" src={my_avatar} />
-          <div className="category">
-          <h3>Age:</h3>
-          <h3>Sexe:</h3>
-          <h3>Taille:</h3>
-          <h3>Poids:</h3>
-          </div>
-          <div className="response">
-          <h3>xx</h3>
-          <h3>xx</h3>
-          <h3>xx</h3>
-          <h3>xx</h3>
-          </div>
-        </div>
+              <h2 className="quiz_title">[insert quiz title]</h2>
+              <img className="my_avatar" src={my_avatar} />
+              <div className="category">
+                <h3>Age:</h3>
+                <h3>Sexe:</h3>
+                <h3>Taille:</h3>
+                <h3>Poids:</h3>
+              </div>
+              <div className="response">
+                <h3>xx</h3>
+                <h3>xx</h3>
+                <h3>xx</h3>
+                <h3>xx</h3>
+              </div>
+            </div>
 
-        <div className="container result2">
-          <h2 className="quiz_title">[insert quiz title]</h2>
-          <img className="my_avatar" src={my_avatar} />
-          <div className="category2">
-          <h3>Age:</h3>
-          <h3>Sex:</h3>
-          <h3>Height:</h3>
-          <h3>Weight:</h3>
-          </div>
-          <div className="response">    
-          <h3>xx</h3>
-          <h3>xx</h3>
-          <h3>xx</h3>
-          </div>
-        </div>
+            <div className="container result2">
+              <h2 className="quiz_title">[insert quiz title]</h2>
+              <img className="my_avatar" src={my_avatar} />
+              <div className="category2">
+                <h3>Age:</h3>
+                <h3>Sex:</h3>
+                <h3>Height:</h3>
+                <h3>Weight:</h3>
+              </div>
+              <div className="response">
+                <h3>xx</h3>
+                <h3>xx</h3>
+                <h3>xx</h3>
+              </div>
+            </div>
 
-        <div className="container result3">
-          <h2 className="quiz_title">[insert quiz title]</h2>
-          <img className="my_avatar" src={my_avatar} />
-          <div className="category3">
-          <h3>Fumeur:</h3>
-          <h3>Alimentation:</h3>
-          <h3>Activité physique:</h3>
-          <h3>Poids:</h3>
-          <h3>Alcool:</h3>
-          </div>
-          <div className="response3">
-            
-          <h3>xx</h3>
-          <h3>xx</h3>
-          <h3>xx</h3>
-          <h3>xx</h3>
-          </div>
-        </div>
+            <div className="container result3">
+              <h2 className="quiz_title">[insert quiz title]</h2>
+              <img className="my_avatar" src={my_avatar} />
+              <div className="category3">
+                <span>
+                  <h3>Fumeur:</h3>
+                  <Slider />
+                </span>
+                <h3>Alimentation:</h3>
+                <h3>Activité physique:</h3>
+                <h3>Poids:</h3>
+                <h3>Alcool:</h3>
+              </div>
+              <div className="response3">
+                <Slider />
+                <h3>xxxxxx</h3>
+                <h3>xx</h3>
+                <h3>xx</h3>
+                <h3>xx</h3>
+              </div>
+            </div>
           </div>
         )}
-
       </div>
     </React.Fragment>
   );
