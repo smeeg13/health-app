f3f39328e8448ae3f610bc9e9dd2b24bae2b4626
smeg <smeg1334@gmail.com>
2022-11-06 15:17:14 +0100

    Circular Bar in resultat

--- src/components/BoxResultat.js
+++ src/components/BoxResultat.js
@@ -1,28 +1,44 @@
 import "../App.css";
 import React from "react";
 import { Slider, Box } from "@mui/material";
-
+import CircularProgressBar from "./CircularProgessBar";
 export default function BoxResultat(props) {
-
   function valuetext(value) {
     return `${value}%`;
   }
-
+  console.log("Value inside maladie : ", props.maladies);
   return (
     <div>
       <div className="container_label3">
-        <label style={{marginTop:"10px"}}  className="label_results">Diabete : </label>
+        <label style={{ marginTop: "10px" }} className="label_results">
+          Diabete :{" "}
+        </label>
         <br></br>
-        <label style={{marginTop:"30px"}}  className="label_results">Cancer : </label>
+        <label style={{ marginTop: "30px" }} className="label_results">
+          Cancer :{" "}
+        </label>
         <br></br>
-        <label style={{marginTop:"30px"}}  className="label_results">Infarctus :</label>
+        <label style={{ marginTop: "30px" }} className="label_results">
+          Infarctus :
+        </label>
         <br></br>
-        <label style={{marginTop:"30px"}}  className="label_results">Non-infarctus :</label>
+        <label style={{ marginTop: "30px" }} className="label_results">
+          Non-infarctus :
+        </label>
       </div>
 
       <div className="container_results">
-      <Box sx={{ width: 150, marginLeft: 10, float:"right", marginRight:"30px"}}>
-        <Slider
+        <Box
+          sx={{
+            width: 150,
+            marginLeft: 10,
+            float: "right",
+            marginRight: "30px",
+          }}
+        >
+          <CircularProgressBar progress={props.maladies.diabete} />
+
+          {/* <Slider
           min="0"
           max="100"
           aria-label="Custom marks"
@@ -32,11 +48,20 @@ export default function BoxResultat(props) {
           valueLabelDisplay="on"
           disabled
           size="medium"
-        />
-      </Box>
+        /> */}
+        </Box>
 
-      <Box  sx={{ width: 150, marginLeft: 10, float:"right", marginRight:"30px"}}>
-        <Slider
+        <Box
+          sx={{
+            width: 150,
+            marginLeft: 10,
+            float: "right",
+            marginRight: "30px",
+          }}
+        >
+          <CircularProgressBar progress={props.maladies.cancer} />
+
+          {/* <Slider
           min="0"
           max="100"
           aria-label="Custom marks"
@@ -46,11 +71,20 @@ export default function BoxResultat(props) {
           valueLabelDisplay="on"
           disabled
           size="medium"
-        />
-      </Box>
+        /> */}
+        </Box>
 
-      <Box sx={{ width: 150, marginLeft: 10, float:"right", marginRight:"30px" }}>
-        <Slider
+        <Box
+          sx={{
+            width: 150,
+            marginLeft: 10,
+            float: "right",
+            marginRight: "30px",
+          }}
+        >
+          <CircularProgressBar progress={props.maladies.infarctus} />
+
+          {/* <Slider
           min="0"
           max="100"
           aria-label="Custom marks"
@@ -60,11 +94,19 @@ export default function BoxResultat(props) {
           valueLabelDisplay="on"
           disabled
           size="medium"
-        />
-      </Box>
+        /> */}
+        </Box>
 
-      <Box sx={{ width: 150, marginLeft: 10, float:"right", marginRight:"30px" }}>
-        <Slider
+        <Box
+          sx={{
+            width: 150,
+            marginLeft: 10,
+            float: "right",
+            marginRight: "30px",
+          }}
+        >
+          <CircularProgressBar progress={props.maladies.nonInfarctus} />
+          {/* <Slider
           min="0"
           max="100"
           aria-label="Custom marks"
@@ -74,9 +116,8 @@ export default function BoxResultat(props) {
           valueLabelDisplay="on"
           disabled
           size="medium"
-        />
-      </Box>
-
+        /> */}
+        </Box>
       </div>
     </div>
   );
